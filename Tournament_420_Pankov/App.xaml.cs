849f3faee19018e540b7265768c7ee1088e0739c
Роберт <@ROBERT>
2024-12-26 13:09:10 +0300

    Добавьте файлы проекта.

---
+++ Tournament_420_Pankov/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Tournament_420_Pankov.Models;
+
+namespace Tournament_420_Pankov
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static TournamentDB_420_PankovEntities DB = new TournamentDB_420_PankovEntities();
+    }
+}
